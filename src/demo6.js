6d4dcb5347b97de051791c64d02b6e240ff35a0d
frakxman <frakxman@hotmail.com>
2020-06-30 14:20:52 -0500

    challenge-javascript-10 Ok

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,32 @@
-const getMaxNumber = (numbersArray) => {
-  // your code here
+var answer
+
+const getMaxNumber = ( numbersArray ) => {
+  answer = numbersArray.reduce(( initialValue, resultValue ) => {
+    if (resultValue > initialValue) {
+      return resultValue;
+    } else {
+      return initialValue;
+    }
+  });
+  return answer;
 }
 
-const getMinNumber = (numbersArray) => {
-  // your code here
+const getMinNumber = ( numbersArray ) => {
+  answer = numbersArray.reduce(( initialValue, resultValue ) => {
+    if (initialValue > resultValue) {
+      return resultValue;
+    } else {
+      return initialValue;
+    }
+  }, numbersArray[0])
+  return answer;
 }
 
-const getAverage = (numbersArray) => {
-  // your code here
+const getAverage = ( numbersArray ) => {
+  answer = numbersArray.reduce(( initialValue, resultValue ) => {
+    return (initialValue + resultValue);
+  }, 0)
+  return answer / numbersArray.length;
 }
 
-module.exports = { getMaxNumber, getMinNumber, getAverage };
+module.exports = { getMaxNumber, getMinNumber, getAverage }
